d18e2fb2d65715ed59d3a39a2ef44e79fcb5665b
ArianaVillegas <ariana.villegas@utec.edu.pe>
2020-11-03 01:34:19 -0500

    Add parser

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main(){
 	
-	parse("data/French.txt","data/process_Frech.txt");
-	parse("data/German.txt","data/process_German.txt");
-	parse("data/Italian.txt","data/process_Italian.txt");
-	parse("data/Portuguese.txt","data/process_Portuguese.txt");
-	parse("data/Spanish.txt","data/process_Spanish.txt");
+	parse("data/French.txt","data/processed_Frech.txt");
+	parse("data/German.txt","data/processed_German.txt");
+	parse("data/Italian.txt","data/processed_Italian.txt");
+	parse("data/Portuguese.txt","data/processed_Portuguese.txt");
+	parse("data/Spanish.txt","data/processed_Spanish.txt");
 	parseLatin("data/Latin.txt","data/processed_Latin.txt");
 	
 	return 0;
